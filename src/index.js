c1bfe706f0be4333b277325beb97de9043602761
kaichi <kaichi1013@g.ecc.u-tokyo.ac.jp>
2025-09-27 14:15:46 +0900

    コンポーネント

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-// コンポーネントの頭文字は大文字で始まり
-const App = () => {
-  return (
-    <div>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
-    </div>
-  );
-};
-
-// <App /> のところで、App関数の中身が表示される
 root.render(
   <StrictMode>
     <App />
