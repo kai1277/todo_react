13c58423dd0368b0aeece85a7bc42bbfd3fa8189
kaichi <kaichi1013@g.ecc.u-tokyo.ac.jp>
2025-09-29 11:59:55 +0900

    イベントとスタイル

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 export const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px",
+    margin: 100,
+  };
   return (
     <div>
-      <h1>こんにちは！</h1>
+      <h1 style={contentStyle}>こんにちは！</h1>
       <p>お元気ですか？</p>
+      <button onCLick={onClickButton}>ボタン</button>
     </div>
   );
 };
